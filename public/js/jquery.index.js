19a143da95cc1adc745e4587fe960f8de7d2183e
muwenzi <mrgaonju@gmail.com>
2015-11-11 00:52:58 +0800

    dev_v2.1.0_nav

--- public/js/jquery.index.js
+++ public/js/jquery.index.js
@@ -3,13 +3,25 @@ $(function () {
 	$('.nav-bar-menu span').hover(
 		function () {
 			$('.nav-bar-layout').show();
+			$(this).index() !== 0 && $('.category ul').eq($(this).index()-1).addClass('ul-active')
 		},
 		function () {
+			$('.nav-bar-layout ul').removeClass('ul-active');
 			$('.nav-bar-layout').hide();
 			$('.nav-bar-layout').hover(
 				function(){$('.nav-bar-layout').show();},
 				function(){$('.nav-bar-layout').hide();}
 			);
-		})
+	});
+	$('.category ul').hover(
+		function(){
+			$(this).addClass('ul-active')
+			$('.nav-bar-menu span').eq($(this).index()+1).addClass('span-hover');
+		},
+		function(){
+			$(this).removeClass('ul-active')
+			$('.nav-bar-menu span').eq($(this).index()+1).removeClass('span-hover');
+		}
+	);
 
 });
