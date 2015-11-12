bfc1358a80229c55c4a65ea3db4fe3b0236b36b8
muwenzi <mrgaonju@gmail.com>
2015-11-12 23:45:07 +0800

    dev_v2.1.0_index

--- public/js/jquery.index.js
+++ public/js/jquery.index.js
@@ -1,5 +1,16 @@
 $(function () {
 
+	$('.nav li').click(function () {
+		$("li").removeAttr("class");
+		$(this).addClass('active')
+	});
+	$('.search-bar').focusin(function () {
+		$(this).find('.search-btn').addClass('search-btn-foucs')
+	});
+	$('.search-bar').focusout(function () {
+		$(this).find('.search-btn').removeClass('search-btn-foucs')
+	});
+
 	$('.nav-bar-menu span').hover(
 		function () {
 			$('.nav-bar-layout').show();
