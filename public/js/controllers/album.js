0efdc7d9cd3c872c2cdc7d45eee468af335dc382
muwenzi <mrgaonju@gmail.com>
2015-11-15 01:47:03 +0800

    dev

--- public/js/controllers/album.js
+++ public/js/controllers/album.js
@@ -1,5 +1,20 @@
 angular.module('album', [])
 	   .controller('album', function ($scope) {
+			$scope.imgs = [
+				{src: 'http://ww2.sinaimg.cn/mw690/0062ZzD9jw1exdpclu82qj32bc1jme83.jpg'},
+				{src: 'http://ww2.sinaimg.cn/mw690/0062ZzD9jw1exdpcutw29j31jm2bc7wj.jpg'},
+				{src: 'http://ww3.sinaimg.cn/mw690/0062ZzD9jw1exdpcfox7wj32bc1jme83.jpg'},
+				{src: 'http://ww3.sinaimg.cn/mw690/0062ZzD9jw1exdpdcqcdcj32bc1jmu0y.jpg'},
+				{src: 'http://ww2.sinaimg.cn/mw690/0062ZzD9jw1exdpcjbadgj31jm2bcb2b.jpg'},
+				{src: 'http://ww2.sinaimg.cn/mw690/0062ZzD9jw1exdpdjpzqhj32bc1jmhdv.jpg'},
+				{src: 'http://ww4.sinaimg.cn/mw690/0062ZzD9jw1exdpd2q4dpj32bc1jm1kz.jpg'},
+				{src: 'http://ww3.sinaimg.cn/mw690/0062ZzD9jw1exdpcq9j3rj31jm2bcb2b.jpg'},
+				{src: 'http://ww3.sinaimg.cn/mw690/0062ZzD9jw1exdpdgmg1lj329f1icu0y.jpg'},
+				{src: 'http://ww2.sinaimg.cn/mw690/0062ZzD9jw1exdpd6mkpgj31jm2bce83.jpg'},
+				{src: 'http://ww3.sinaimg.cn/mw690/0062ZzD9jw1exdpcyi3anj31jm2bcqv7.jpg'},
+				{src: 'http://ww2.sinaimg.cn/mw690/0062ZzD9jw1exdpdpi15oj31jm2bckjn.jpg'},
+				{src: 'http://ww3.sinaimg.cn/mw690/0062ZzD9jw1exdpdacjaej32bc1awe82.jpg'},
+			];
 
 	   })
 	   .controller('album-nav', function ($scope) {
