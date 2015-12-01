4b5e5092c43d33e7dbeba898b5f5cd61e2855816
muwenzi <mrgaonju@gmail.com>
2015-12-01 20:47:22 +0800

    dev

--- public/js/controllers/album.js
+++ public/js/controllers/album.js
@@ -1,61 +1,34 @@
-angular.module('album', [])
-	.controller('album', ['$scope', '$location', '$anchorScroll', function ($scope, $location, $anchorScroll) {
-		$scope.info = {
-			id: '1001',
-			name: '《后棠》',
-			cover: 'img//album/1001/0.jpg',
-			belong: '人像写真--其他',
-			model: '@耳东耳东LION',
-			//				device: 'Canon 5DMarkIII 35/1.4L',
-			date: '2015-11-5',
-			place: '南京·咖啡馆',
-			author: '@摄影师羊驼',
-			description: '如果，有醒不了的梦，我一定去做。如果，有走不完的路，我一定去走。如果，有变不了的爱，我一定去求。让懂的人懂，让不懂的人不懂，让世界是世界，我甘心是我的茧。',
-			by: '——简媜《美丽的茧》'
+angular.module('album', ['ngRoute'])
+	// 自定义指令
+	.directive('my-afterDom', function ($timeout) {
+		return {
+			restrict: 'A',
+			link: function (scope, element, attr) {
+				if (scope.$last === true) {
+					$timeout(function () {
+						scope.$emit('ngRepeatFinished');
+					});
+				}
+			}
 		};
-		$scope.imgs = [
-			{
-				src: 'img/album/1001/1.jpg'
-			},
-			{
-				src: 'img/album/1001/2.jpg'
-			},
-			{
-				src: 'img/album/1001/3.jpg'
-			},
-			{
-				src: 'img/album/1001/4.jpg'
-			},
-			{
-				src: 'img/album/1001/5.jpg'
-			},
-			{
-				src: 'img/album/1001/6.jpg'
-			},
-			{
-				src: 'img/album/1001/7.jpg'
-			},
-			{
-				src: 'img/album/1001/8.jpg'
-			},
-			{
-				src: 'img/album/1001/9.jpg'
-			},
-			{
-				src: 'img/album/1001/10.jpg'
-			},
-			{
-				src: 'img/album/1001/11.jpg'
-			},
-			{
-				src: 'img/album/1001/12.jpg'
-			},
-			{
-				src: 'img/album/1001/13.jpg'
-			},
-			];
+	})
+	// 启动前路由配置
+	.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+		$routeProvider
+			.when('/:id', {
+				templateUrl: 'views/common/album_content.html',
+				controller: 'album_content',
+			})
+			.otherwise({
+				redirectTo: '/1001'
+			});
+	}])
+	.run(function ($rootScope, $routeParams, $location, $anchorScroll) {
+		// 变量初始化
+		$rootScope.album_content = {};
+
 		// 页面锚点解决方案
-		$scope.goto = function (_id) {
+		$rootScope.goto = function (_id) {
 			// set the location.hash to the id of
 			// the element you wish to scroll to.
 			$location.hash(_id);
@@ -65,24 +38,34 @@ angular.module('album', [])
 		};
 		// 单个相册侧边栏滚动悬浮
 		$(function () {
-				$('.nav-side').stickUp({
-					//							topMargin : '55px'
-					parts: {
-						0: 'intro',
-						1: 'imgFirst',
-						2: 'imgFirst',
-						3: 'footer'
-					},
-					itemHover: ''
-				});
-			})
+			$('.nav-side').stickUp({
+				//							topMargin : '55px'
+				parts: {
+					0: 'intro',
+					1: 'imgFirst',
+					2: 'imgFirst',
+					3: 'footer'
+				},
+				itemHover: ''
+			});
+		});
 		// 幻灯片浏览
 		$(function () {
 			$(".boxer").boxer({
 				mobile: true
 			});
-		})
-	}])
+		});
+	})
+	.controller('album_content', function ($rootScope, $scope, $http, $routeParams) {
+
+		$http.get("/json/album_content.json")
+			.success(function (_data) {
+				_.each(_data, function (element) {
+					element.id === $routeParams.id && ($rootScope.album_content = element);
+				});
+
+			});
+	})
 
 .controller('album-nav', function ($scope) {
 
