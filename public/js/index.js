bd955b5db4d6c5cf9233551c24b329455ac34c73
muwenzi <mrgaonju@gmail.com>
2015-12-02 21:17:56 +0800

    dev

--- public/js/index.js
+++ public/js/index.js
@@ -201,15 +201,10 @@ angular.module('index', []).controller('indexController', function ($scope, $htt
 		}]
 	};
 	$scope.allLists.push($scope.people, $scope.film, $scope.city);
-	//	$http.get("http://218.244.148.188/json/albumList.json")
-	//		 .success(function (_data) {
-	//			var j = 0;
-	//		    for (var i = 0; i < 4; i++) {
-	//				j = i + 4;
-	//				$scope.albumList1[i] = _data[i];
-	//				$scope.albumList2[i] = _data[j];
-	//			}
-	//		});
+	$http.get("/json/album_category.json")
+		 .success(function (_data) {
+			$scope.classItems = _data;
+		});
 
 }).controller('album-nav', function ($scope) {
 
