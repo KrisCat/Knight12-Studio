121a9e66ae4d4d4dc60fd2455f3ea5297592e835
muwenzi <mrgaonju@gmail.com>
2015-12-04 12:20:08 +0800

    dev_首页相册个数适配JS

--- public/js/index.js
+++ public/js/index.js
@@ -4,243 +4,89 @@ angular.module('index', [])
 		return {
 			restrict: 'A',
 			// 链接函数将作用域和dom进行链接
-			link: function(scope,element,attr) {
-            if (scope.$last === true) {
-                $timeout(function() {
-                    //根据controller的关系是选择$emit或者$broadcast
-                    scope.$emit('ngRepeatFinished');
-                });
-            }
-        }
+			link: function (scope, element, attr) {
+				if (scope.$last === true) {
+					$timeout(function () {
+						//根据controller的关系是选择$emit或者$broadcast
+						scope.$emit('ngRepeatFinished');
+					});
+				}
+			}
 		};
 	})
-	.controller('indexController', function ($scope, $http) {
+	.controller('indexController', function ($scope, $http, $q) {
+		/**
+		 * 轮播图切换
+		 * 每隔4s切换一次，无限循环
+		 * hover触发切换事件
+		 */
+		$(function () {
+			var bannerSlider = new Slider($('#banner_tabs'), {
+				time: 5000,
+				delay: 400,
+				event: 'hover',
+				auto: true,
+				mode: 'fade',
+				controller: $('#bannerCtrl'),
+				activeControllerCls: 'active'
+			});
+			$('#banner_tabs .flex-prev').click(function () {
+				bannerSlider.prev();
+			});
+			$('#banner_tabs .flex-next').click(function () {
+				bannerSlider.next();
+			});
+		});
+		$q.all({
+			req1: $http.get("/json/people_list.json"),
+			req2: $http.get("/json/scenery_list.json"),
+			req3: $http.get("/json/humanity_list.json"),
+			req4: $http.get("/json/impression_list.json")
+		}).then(function (arr) {
+			// 以上请求都完成后执行以下代码
+			_.each(arr, function (e) {
+				e.data.name === '人像摄影' && ($scope.people = e.data);
+				e.data.name === '风光摄影' && ($scope.scenery = e.data);
+				e.data.name === '人文摄影' && ($scope.humanity = e.data);
+				e.data.name === '印象系列' && ($scope.impression = e.data);
+			});
+			/**
+			 * 大屏排列12个，小屏排列10个
+			 * @threshold  阈值
+			 * @return 选取后的新数组
+			 */
+			function select(arr, threshold) {
+				arr.lists = _(arr.lists).chain()
+									    .map(function (e, i) {
+											if (i < threshold) return e;
+										 })
+									    .compact()
+									    .value();
+			}
+			if ($(window).width() >= 1400) {
+				select($scope.people, 12);
+				select($scope.scenery, 12);
+				select($scope.humanity, 12);
+				select($scope.impression, 12);
+			} else {
+				select($scope.people, 10);
+				select($scope.scenery, 10);
+				select($scope.humanity, 10);
+				select($scope.impression, 10);
+			}
+			// 加工过后的数据
+			$scope.allLists = [];
+			$scope.allLists.push($scope.people, $scope.scenery, $scope.humanity, $scope.impression);
+		});
 
-	/**
-	 * 轮播图切换
-	 * @note  每隔30s切换一次，无限循环
-	 * @return {Void}
-	 */
-	$(function() {
-		var bannerSlider = new Slider($('#banner_tabs'), {
-			time: 5000,
-			delay: 400,
-			event: 'hover',
-			auto: true,
-			mode: 'fade',
-			controller: $('#bannerCtrl'),
-			activeControllerCls: 'active'
-		});
-		$('#banner_tabs .flex-prev').click(function() {
-			bannerSlider.prev()
-		});
-		$('#banner_tabs .flex-next').click(function() {
-			bannerSlider.next()
-		});
-	})
-
-	$scope.allLists = [];
-	$scope.people = {
-		name: '人像写真',
-		lists: [{
-			id: 001,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1exdpcutw29j31jm2bc7wj.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 002,
-			cover: 'http://ww1.sinaimg.cn/thumb150/0062ZzD9gw1exs9sder84j31jm2bckjo.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 003,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1exnkbrvxodj31jm2bc7wk.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 004,
-			cover: 'http://ww3.sinaimg.cn/thumb150/0062ZzD9jw1ex8l94a60wj32bc1jm7wj.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 005,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1ex7x2jwwphj32bc1jme82.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 006,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1ewzpq4z6w3j32bc1jmu0y.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 007,
-			cover: 'http://ww4.sinaimg.cn/thumb150/0062ZzD9jw1ewayxa94ypj31jl2bchdt.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 008,
-			cover: 'http://ww3.sinaimg.cn/thumb150/0062ZzD9jw1evu30gznwjj3226334qv8.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 009,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1evksv5r3lmj3226334u0z.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 010,
-			cover: 'http://ww4.sinaimg.cn/thumb150/0062ZzD9jw1ev7nrnedpoj32bc1jmkjo.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}]
-	};
-	$scope.film = {
-		name: '胶片味道',
-		lists: [{
-			id: 001,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1exdpcutw29j31jm2bc7wj.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 002,
-			cover: 'http://ww1.sinaimg.cn/thumb150/0062ZzD9gw1exs9sder84j31jm2bckjo.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 003,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1exnkbrvxodj31jm2bc7wk.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 004,
-			cover: 'http://ww3.sinaimg.cn/thumb150/0062ZzD9jw1ex8l94a60wj32bc1jm7wj.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 005,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1ex7x2jwwphj32bc1jme82.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 006,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1ewzpq4z6w3j32bc1jmu0y.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 007,
-			cover: 'http://ww4.sinaimg.cn/thumb150/0062ZzD9jw1ewayxa94ypj31jl2bchdt.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 008,
-			cover: 'http://ww3.sinaimg.cn/thumb150/0062ZzD9jw1evu30gznwjj3226334qv8.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 009,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1evksv5r3lmj3226334u0z.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 010,
-			cover: 'http://ww4.sinaimg.cn/thumb150/0062ZzD9jw1ev7nrnedpoj32bc1jmkjo.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}]
-	};
-	$scope.city = {
-		name: '城市印象',
-		lists: [{
-			id: 001,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1exdpcutw29j31jm2bc7wj.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 002,
-			cover: 'http://ww1.sinaimg.cn/thumb150/0062ZzD9gw1exs9sder84j31jm2bckjo.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 003,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1exnkbrvxodj31jm2bc7wk.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 004,
-			cover: 'http://ww3.sinaimg.cn/thumb150/0062ZzD9jw1ex8l94a60wj32bc1jm7wj.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 005,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1ex7x2jwwphj32bc1jme82.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 006,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1ewzpq4z6w3j32bc1jmu0y.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 007,
-			cover: 'http://ww4.sinaimg.cn/thumb150/0062ZzD9jw1ewayxa94ypj31jl2bchdt.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 008,
-			cover: 'http://ww3.sinaimg.cn/thumb150/0062ZzD9jw1evu30gznwjj3226334qv8.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 009,
-			cover: 'http://ww2.sinaimg.cn/thumb150/0062ZzD9jw1evksv5r3lmj3226334u0z.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}, {
-			id: 010,
-			cover: 'http://ww4.sinaimg.cn/thumb150/0062ZzD9jw1ev7nrnedpoj32bc1jmkjo.jpg',
-			count: '12张',
-			title: '南方姑娘',
-			date: '2014年11月8日'
-		}]
-	};
-	$scope.allLists.push($scope.people, $scope.film, $scope.city);
-	$http.get("/json/album_category.json")
-		.success(function (_data) {
-			$scope.classItems = _data;
-		});
-	$scope.$on('ngRepeatFinished', function () {
+		$http.get("/json/album_category.json")
+			.success(function (_data) {
+				$scope.classItems = _data;
+			});
+		$scope.$on('ngRepeatFinished', function () {
 			// 下面是在dom render完成后执行的js
 			// 所有相册分类类目展示切换
-			$('.album-category li').click(function() {
+			$('.album-category li').click(function () {
 				console.log('ok');
 			})
 			var _li_this = '';
@@ -256,20 +102,20 @@ angular.module('index', [])
 					$('#j-category-view').hide();
 					$('#j-category-view li').eq(_li_this).hide();
 					$('#j-category-view').hover(
-						function() {
+						function () {
 							$('.album-category li').eq(_li_this).addClass('active');
 							$(this).show();
 							$('#j-category-view li').eq(_li_this).show()
 						},
-						function() {
+						function () {
 							$(this).hide();
 							$('#j-category-view li').eq(_li_this).hide();
 							$('.album-category li.active').removeClass('active');
 						}
 					)
-			});
+				});
 		});
 
-}).controller('album-nav', function ($scope) {
+	}).controller('album-nav', function ($scope) {
 
-});
+	});
