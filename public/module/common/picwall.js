919fab5ac771936bf2e8362db73e92fce26bed12
muwenzi <mrgaonju@gmail.com>
2015-12-22 23:54:37 +0800

    dev

--- public/module/common/picwall.js
+++ public/module/common/picwall.js
@@ -11,7 +11,7 @@ var f = function (angular, tpl) {
 
 	//angular会自动根据controller函数的参数名，导入相应的服务
 	return {
-		controller: function ($scope, $stateParams, $http, $interval, $q) {
+		controller: function ($scope, $http, $interval, $q) {
 			$scope.state = [1, 0, 0, 0, 0];
 			$scope.showPicWall = _.shuffle($scope.showPicWall);
 		},
