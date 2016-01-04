8dbe80726daa730179abc17708b4cb3f64e66eda
muwenzi <mrgaonju@gmail.com>
2016-01-04 10:32:05 +0800

    dev_调整sass文件夹结构，mv集成开发

--- public/module/about/aboutCtrl.js
+++ public/module/about/aboutCtrl.js
@@ -2,19 +2,15 @@
  * ------------------------------------------
  * 关于我们（about）控制器
  * @version  1.0
- * @update   2015/12/14
- * @author   cisheng(mrgaonju@gmail.com)
+ * @update   2016/01/04
+ * @author   小木瓜(mrgaonju@gmail.com)
  * ------------------------------------------
  */
 
-var f = function (angular, tpl) {
+var f = function () {
+	return function ($scope) {
 
-	//angular会自动根据controller函数的参数名，导入相应的服务
-	return function ($scope, $http, $interval, $q) {
-
-		}
+	}
 };
 
-define([
-	'angular'
-], f)
+define(['angular'], f)
