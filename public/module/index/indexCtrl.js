df3c33c541c6e7b42a1c9ee8a9ecb44fea193a9f
muwenzi <mrgaonju@gmail.com>
2016-01-06 17:56:26 +0800

    dev_风光摄影头部样式设计and解决首页轮播图fade和指示灯bug

--- public/module/index/indexCtrl.js
+++ public/module/index/indexCtrl.js
@@ -17,15 +17,15 @@ var f = function () {
 			 * hover触发切换事件
 			 */
 //			$(function () {
-			Slider($('#banner_tabs'), {time: 5000
-				                      ,delay: 400
-				                      ,event: 'hover'
-				                      ,auto: true
-				                      ,mode: 'fade'
-				                      ,controller: $('#bannerCtrl')
-				                      ,activeControllerCls: 'active'
-									  }
-				  );
+//			Slider($('#banner_tabs'), {time: 5000
+//				                      ,delay: 400
+//				                      ,event: 'hover'
+//				                      ,auto: true
+//				                      ,mode: 'fade'
+//				                      ,controller: $('#bannerCtrl')
+//				                      ,activeControllerCls: 'active'
+//									  }
+//				  );
 //				$('#banner_tabs .flex-prev').click(function () {
 //					bannerSlider.prev();
 //				});
--- public/module/scenery/sceneryCtrl.js
+++ public/module/scenery/sceneryCtrl.js
@@ -9,7 +9,7 @@
 
 var f = function () {
 	return function ($scope, $http) {
-
+		$scope.state = [1, 0, 0, 0, 0];
 	}
 };
 
