7c017077c846c3650c17ef072baf5890a45c5498
muwenzi <mrgaonju@gmail.com>
2016-01-11 11:24:41 +0800

    dev_新增摄影世界板块

--- public/module/love/loveCtrl.js
+++ public/module/love/loveCtrl.js
@@ -7,10 +7,31 @@
  * ------------------------------------------
  */
 
-var f = function (angular, tpl) {
-	return function ($scope, $http, $rootScope) {
+var f = function () {
+	return function ($scope, $rootScope, $http, $stateParams, $sce) {
 		$rootScope.navState = [0, 0, 0, 0, 0, 1, 0];
+		var _map = {'nanking':     [1, 0, 0]
+			       ,'hangzhou':    [0, 1, 0]
+			       ,'honglou':     [0, 0, 1]
+		           };
+		var _url = "/json/humanity_" + $stateParams.type + '.json'
+		$scope.state = _map[$stateParams.type];
+		$http.get(_url)
+			 .success(function (_data) {
+				 $scope.data = _data;
+				 $scope.data.description = $sce.trustAsHtml(_data.description);
+			 });
+		$scope.$on('ngRepeatFinished', function () {
+			// 下面是在dom render完成后执行的js
+			// 幻灯片浏览
+			$(function () {
+				$(".boxer").boxer({
+					mobile: true
+				});
+			});
+		});
 	}
 };
 
 define(['angular'], f)
+
