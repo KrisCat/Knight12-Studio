f7e4c30320dc58f27027c6f601bcd3ac35660c2f
muwenzi <mrgaonju@gmail.com>
2016-03-02 18:01:47 +0800

    bugfix_人像图片墙

--- public/component/picwall/picwallCtrl.js
+++ public/component/picwall/picwallCtrl.js
@@ -18,15 +18,18 @@ var f = function () {
 				$http.get(_jsonUrl)
 					.success(function (_data) {
 						//var _len = _data.length;
-						var _newDate = [];
-						for(var i = 0; i < 2; i++) {
-							_newDate.push(_data[i]);
-						}
+						//var _newDate = [];
+						//for(var i = 0; i < 5; i++) {
+						//	_newDate.push(_data[i]);
+						//}
 						$scope.showPicwall = [];
-						_.each(_newDate, function (e1) {
+						_.each(_data, function (e1) {
 								_.each(e1.imgs, function (e2) {
-									e2.src = '../../' + e2.src;
-									$scope.showPicwall.push(e2.src);
+									if (e2.picwall === 1) {
+										e2.src = '../../' + e2.src;
+										e2.src = e2.src.replace(/.jpg/, '_picwall.jpg');
+										$scope.showPicwall.push(e2.src);
+									}
 								})
 							});
 							// $scope.showPicwall = _.shuffle($scope.showPicwall);
@@ -39,32 +42,32 @@ var f = function () {
 		whichType('love');
 		$scope.$on('ngRepeatFinished', function () {
 			var $container = $('.masonry');
-			$container.imagesLoaded(function () {
-					$container.masonry({
-						gutterWidth: 1,
-						gutterHeight: 1,
-						itemSelector: '.item',
-						isAnimated: true,
-					});
-				})
-			// 图片滚动延迟加载
-			$(".masonry .item img").lazyload({
-				effect: "fadeIn"
-			});
-			var imgNum = $('.masonry .item img').length;
-			$('.masonry .item img').load(function () {
-				if (!--imgNum) {
-					setTimeout(function () {
-						$('.loading').hide();
-						$container.show();
-					}, 400)
-				}
-			});
+		//	$container.imagesLoaded(function () {
+		//			$container.masonry({
+		//				gutterWidth: 1,
+		//				gutterHeight: 1,
+		//				itemSelector: '.item',
+		//				isAnimated: true,
+		//			});
+		//		})
+		//	// 图片滚动延迟加载
+		//	$(".masonry .item img").lazyload({
+		//		effect: "fadeIn"
+		//	});
+		//	var imgNum = $('.masonry .item img').length;
+		//	$('.masonry .item img').load(function () {
+		//		if (!--imgNum) {
+		//			setTimeout(function () {
+		//				$('.loading').hide();
+		//				$container.show();
+		//			}, 400)
+		//		}
+		//	});
 		})
 	}
 };
 
 define(['angular'
-	   , 'jquery.ui.masonry'
-	   , 'jquery.ui.lazyload'
+	   //, 'jquery.ui.masonry'
+	   //, 'jquery.ui.lazyload'
 ], f);
