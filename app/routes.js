2a8b7dd8fa1dc1f8eed6df583d79164fcb3f3d0e
Kimi <mrgaonju@gmail.com>
2016-05-06 14:57:13 +0800

    dev_修改顶部导航

--- app/routes.js
+++ app/routes.js
@@ -6,8 +6,16 @@ module.exports = function(app) {
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
-	app.get('*', function(req, res) {
+	// 主页
+	app.get('/', function(req, res) {
 		res.sendfile('./public/index.html');
 	});
-
+	// 电影单
+	app.get('/film', function(req, res) {
+		res.sendfile('./public/film/001.html');
+	});
+	// 图片后台系统
+	app.get('/login', function(req, res) {
+		res.sendfile('./public/admin/login.html');
+	});
 };
