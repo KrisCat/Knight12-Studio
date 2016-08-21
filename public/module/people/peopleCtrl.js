9198caa87b82cec0b58c5161fbc97796ca87db93
muwenzi <mrgaonju@gmail.com>
2016-08-21 11:55:27 +0800

    dev_ mend the right click

--- public/module/people/peopleCtrl.js
+++ public/module/people/peopleCtrl.js
@@ -9,23 +9,23 @@
 
 var f = function () {
 	return function ($scope, $http, $rootScope, $stateParams) {
-			$rootScope.toTop();
-			$rootScope.navState = [0, 1, 0, 0, 0, 0, 0];
-			$scope.state = [1, 0, 0, 0, 0, 0];
-			$scope.activeTypeConfirm = function (_index) {
-				$scope.state = _.map($scope.state, function () {
-					return 0;
-				});
-				$scope.state[_index] = 1;
-			};
-			$scope.isActive = function () {
-				$stateParams.type === 'all' && $scope.activeTypeConfirm(1);
-				$stateParams.type === 'environment' && $scope.activeTypeConfirm(2);
-				$stateParams.type === 'black' && $scope.activeTypeConfirm(3);
-				$stateParams.type === 'film' && $scope.activeTypeConfirm(4);
-				$stateParams.type === 'other' && $scope.activeTypeConfirm(5);
-			};
-		}
+		$rootScope.toTop();
+		$rootScope.navState = [0, 1, 0, 0, 0, 0, 0];
+		$scope.state = [1, 0, 0, 0, 0, 0];
+		$scope.activeTypeConfirm = function (_index) {
+			$scope.state = _.map($scope.state, function () {
+				return 0;
+			});
+			$scope.state[_index] = 1;
+		};
+		$scope.isActive = function () {
+			$stateParams.type === 'all' && $scope.activeTypeConfirm(1);
+			$stateParams.type === 'environment' && $scope.activeTypeConfirm(2);
+			$stateParams.type === 'black' && $scope.activeTypeConfirm(3);
+			$stateParams.type === 'film' && $scope.activeTypeConfirm(4);
+			$stateParams.type === 'other' && $scope.activeTypeConfirm(5);
+		};
+	}
 };
 
 define(['angular'], f);
